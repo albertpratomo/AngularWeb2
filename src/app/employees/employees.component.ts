55c82e83a5d3b4772d7c865b9550987b425d48f6
Shi,Chao C <c.shi@student.fontys.nl>
2018-09-17 14:40:14 +0200

    update

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -26,16 +26,16 @@ export class EmployeesComponent implements OnInit {
 
    
    employees: Employee[] = [
-  { id: 11, name: 'Mr. Nice',email:'coco@gmail.com',phone:'1234567890'},
-  { id: 12, name: 'Narco', email:'coco@gmail.com',phone:'1234567890'},
-  { id: 13, name: 'Bombasto', email:'coco@gmail.com',phone:'1234567890'},
-  { id: 14, name: 'Celeritas', email:'coco@gmail.com',phone:'1234567890'},
-  { id: 15, name: 'Magneta', email:'coco@gmail.com',phone:'1234567890'},
-  { id: 16, name: 'RubberMan', email:'coco@gmail.com',phone:'1234567890'},
-  { id: 17, name: 'Dznama', email:'coco@gmail.com',phone:'1234567890'},
-  { id: 18, name: 'Dr IQ', email:'coco@gmail.com',phone:'1234567890'},
-  { id: 19, name: 'Magma', email:'coco@gmail.com',phone:'1234567890'},
-  { id: 20, name: 'Tornado', email:'coco@gmail.com',phone:'1234567890'}
+  { id: 1, name: 'Mr. Nice',email:'coco@gmail.com',phone:'1234567890'},
+  { id: 2, name: 'Narco', email:'coco@gmail.com',phone:'1234567890'},
+  { id: 3, name: 'Bombasto', email:'coco@gmail.com',phone:'1234567890'},
+  { id: 4, name: 'Celeritas', email:'coco@gmail.com',phone:'1234567890'},
+  { id: 5, name: 'Magneta', email:'coco@gmail.com',phone:'1234567890'},
+  { id: 6, name: 'RubberMan', email:'coco@gmail.com',phone:'1234567890'},
+  { id: 7, name: 'Dznama', email:'coco@gmail.com',phone:'1234567890'},
+  { id: 8, name: 'Dr IQ', email:'coco@gmail.com',phone:'1234567890'},
+  { id: 9, name: 'Magma', email:'coco@gmail.com',phone:'1234567890'},
+  { id: 10, name: 'Tornado', email:'coco@gmail.com',phone:'1234567890'}
 ];
 
 co:number = this.employees.length;
