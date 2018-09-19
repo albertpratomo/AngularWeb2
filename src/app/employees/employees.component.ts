6ea59a69883e4a89becae55f0c160776ade65b16
Shi,Chao C <c.shi@student.fontys.nl>
2018-09-19 16:48:54 +0200

    employees service

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Employee } from '../employee';
+import { Component, OnInit, Input } from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { EmployeeService } from '../employee.service';
+import { Employee } from '../employee';
 
 @Component({
   selector: 'app-employees',
@@ -9,13 +10,19 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 })
 export class EmployeesComponent implements OnInit {
 
-  constructor(private modalService: NgbModal) { }
+  constructor(private modalService: NgbModal ,  private employeeService: EmployeeService) { }
 
   newName:string;
   newEmail:string;
   newPhone:string;
+  employees: Employee[];
+  selectedEmployee: Employee;
+  co:number;
 
   ngOnInit() {
+    this.getEmployeesFromService();
+    this.co = this.employees.length;
+    this.selectedEmployee= this.employees[0];
   }
 
   open(content:any) {
@@ -24,33 +31,21 @@ export class EmployeesComponent implements OnInit {
     });
   }
 
+getEmployeesFromService(): void {
+    this.employeeService.getEmployees().subscribe(employees => this.employees = employees);
+  }
    
-   employees: Employee[] = [
-  { id: 1, name: 'Mr. Nice',email:'coco@gmail.com',phone:'1234567890'},
-  { id: 2, name: 'Narco', email:'coco@gmail.com',phone:'1234567890'},
-  { id: 3, name: 'Bombasto', email:'coco@gmail.com',phone:'1234567890'},
-  { id: 4, name: 'Celeritas', email:'coco@gmail.com',phone:'1234567890'},
-  { id: 5, name: 'Magneta', email:'coco@gmail.com',phone:'1234567890'},
-  { id: 6, name: 'RubberMan', email:'coco@gmail.com',phone:'1234567890'},
-  { id: 7, name: 'Dznama', email:'coco@gmail.com',phone:'1234567890'},
-  { id: 8, name: 'Dr IQ', email:'coco@gmail.com',phone:'1234567890'},
-  { id: 9, name: 'Magma', email:'coco@gmail.com',phone:'1234567890'},
-  { id: 10, name: 'Tornado', email:'coco@gmail.com',phone:'1234567890'}
-];
-
-co:number = this.employees.length;
 
 
-	selectedEmployee: Employee = this.employees[0];
 
 	onSelect(e: Employee): void {
 		this.selectedEmployee = e;
 	}
 
 createEmployee():void {
-    let co = this.employees
+    
     let e: Employee = {
-      id: this.co+1,
+      id: this.employees[this.co-1].id+1,
       name: this.newName,
       email: this.newEmail,
       phone: this.newPhone
@@ -60,7 +55,7 @@ createEmployee():void {
 
   deleteEmployee(i: number): void{
     this.employees.splice(i,1);
-    this.co--;
+    this.co = this.employees.length;
   }
 
 
