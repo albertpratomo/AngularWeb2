714a6bad846ff2b7fcae2fdec5486a73d26bde7b
Albert Pratomo <i406448@fhict.nl>
2018-10-03 13:39:24 +0200

    Added search feature for Dashboard and Projects.

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -1,14 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
-  name: 'filter'
+	name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
-  transform(items: any[], searchText: string): any[] {
-    if(!items) return [];
-    if(!searchText) return items;
-searchText = searchText.toLowerCase();
-return items.filter( it => {
-      return it.toLowerCase().includes(searchText);
-    });
-   }
+	transform(items: any[], searchText: string): any[] {
+		if(!items) return [];
+		if(!searchText) return items;
+		
+		searchText = searchText.toLowerCase();
+		return items.filter( it => {
+			if (it.name) {
+				return it.name.toLowerCase().includes(searchText);
+			} else if (it.title) {
+				return it.title.toLowerCase().includes(searchText);
+			}
+		});
+	}
 }
